783ec32bffd29fcc0f16a28dc8bda10bf5b8b16a
NecromancerMidas <115973453+NecromancerMidas@users.noreply.github.com>
2022-10-26 10:49:35 +0200

    Script mostly done.

--- Script1.js
+++ Script1.js
@@ -1,4 +1,36 @@
 // JavaScript source code
-function imageclick() {
-    document.getElementById("box").innerHTML += '<img src="img/money.png"/>';
+//function imageclick() {
+    //document.getElementById("box").innerHTML += '<img src="img/money.png"/>';}
+
+//let theBag = document.getElementById('bag');
+
+let bag = ""; //Holds the inner.HTML
+let shown = false
+
+function getloot(loot) {
+    bag += loot; //adds the inner.HTML
+    console.log(getloot);
 }
+
+function showlootbag() {
+     document.getElementById('inv').innerHTML = bag;//ads bag content to the div inv innerhtml
+    shown = true
+
+}
+
+function hidebag() {
+    document.getElementById('inv').innerHTML = ('');
+    shown = false
+}
+function thief() {
+    if (shown === true) {
+        hidebag()
+    }
+    else {
+        showlootbag()
+    }
+
+}
+
+
+
